5f35eeae488c683542c63942dd6a849615ff06c3
Dan Hall <daniel.hall@ans.co.uk>
2023-12-06 10:10:34 +0000

    Add startup command

---
+++ scripts/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script runs the post build script commands needed to run the application
+
+cd /home/site/wwwroot || exit 1
+php artisan migrate --force >> /home/startup.log 2>&1
+wget -O /home/site/wwwroot/ssl/DigiCertGlobalRootG2.crt.pem https://cacerts.digicert.com/DigiCertGlobalRootG2.crt.pem >> /home/startup.log 2>&1
+cp /home/site/wwwroot/default /etc/nginx/sites-available/default && service nginx reload >> /home/startup.log 2>&1
